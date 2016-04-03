7e543d9440d7c12e76023eb6741e53511d9b8d51
jakebrady5 <jakebrady5@gmail.com>
2016-04-03 00:18:03 -0400

    initial setup

---
+++ app/controllers/api/v1/recipes_controller.rb
@@ -0,0 +1,45 @@
+module Api
+  module V1
+    class RecipesController < ApplicationController
+
+      skip_before_filter :verify_authenticity_token
+      respond_to :json
+
+      def index
+        respond_with(Recipe.all.order("id DESC"))
+      end
+
+      def show
+        respond_with(Recipe.find(params[:id]))
+      end
+
+      def create
+        @recipe = Recipe.new(recipe_params)
+        if @recipe.save
+          respond_to do |format|
+            format.json {render :json => @recipe}
+          end
+        end
+      end
+
+      def update
+        @recipe = Recipe.find(params[:id])
+        if @recipe.update(recipe_params)
+          respond_to do |format|
+            format.json {render :json => @recipe}
+          end
+        end
+      end
+
+      def destroy
+        respond_with Recipe.destroy(params[:id])
+      end
+
+      private
+        def recipe_params
+          params.require(:recipe).permit(:title)
+        end
+
+    end
+  end
+end
