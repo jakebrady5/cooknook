23af732f3f1f66a3bb6fd1872cf46589f1164969
jakebrady5 <jakebrady5@gmail.com>
2016-04-05 23:29:22 -0400

    installing components

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source 'https://rubygems.org'
 
+gem 'devise'
 gem 'angular-rails-templates'
 gem 'responders'
 gem 'bower-rails'
--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,7 @@ Rails.application.routes.draw do
   root 'application#index'
 
   Rails.application.routes.draw do
+  devise_for :users
     namespace :api, defaults:{format: :json} do
       namespace :v1 do
         resources :recipes
